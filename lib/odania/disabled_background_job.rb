65db178e8e354a646e6cd66431a656a48aef5e77
Mike Petersen <mike@odania-it.de>
2014-05-13 10:13:16 +0200

    Added error catching for background jobs

--- lib/odania/disabled_background_job.rb
+++ lib/odania/disabled_background_job.rb
@@ -1,7 +1,15 @@
 # This is meant as a fallback/test background processor. It directly executes the action.
-module DisabledBackgroundJob
-	def self.enqueue(background_type, opts)
-		puts "Directly processing: #{background_type} with options: #{opts}"
-		background_type.to_s.constantize.new.perform(*opts)
+module Odania
+	module DisabledBackgroundJob
+		def self.enqueue(background_type, opts)
+			# Background jobs should not interfere with the server response
+			begin
+				Rails.logger.info "Directly processing: #{background_type} with options: #{opts}"
+				background_type.to_s.constantize.new.perform(*opts)
+			rescue => e
+				Rails.logger.error "Error in background_job: #{background_type}"
+				Rails.logger.error e
+			end
+		end
 	end
 end
