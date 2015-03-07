9517136e7caf344c29623c62776ee582c9b4b063
Mike Petersen <mike@odania-it.de>
2015-03-07 15:02:35 +0100

    Added social links to footer

--- app/controllers/admin/api/sites_controller.rb
+++ app/controllers/admin/api/sites_controller.rb
@@ -51,7 +51,7 @@ class Admin::Api::SitesController < Admin::ApiController
 	def site_params
 		params.require(:site).permit(:name, :host, :is_active, :is_default, :tracking_code, :description, :user_signup_allowed,
 														:default_from_email, :notify_email_address, :imprint, :terms_and_conditions,
-														:default_widget_id, :redirect_to_id, :template, :default_language_id)
+														:default_widget_id, :redirect_to_id, :template, :default_language_id, social: [:linked_in, :facebook, :google_plus, :twitter])
 	end
 
 	def update_languages
