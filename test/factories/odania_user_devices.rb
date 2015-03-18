ac6d51c6374d16fd22d08b36cd764ba1e005851a
Mike Petersen <mike@odania-it.de>
2015-03-18 15:33:20 +0100

    Updated factory name

--- test/factories/odania_user_devices.rb
+++ test/factories/odania_user_devices.rb
@@ -1,10 +1,11 @@
 FactoryGirl.define do
-	factory :odania_user_device, :class => 'Odania::UserDevice' do
+	factory :user_device, :class => 'Odania::UserDevice' do
 		user
 		platform 'Android'
 		uuid '1234567890'
 		model 'Samsung Galaxy S4'
 		version '4.2'
+		token {SecureRandom.hex(64)}
 	end
 
 end
