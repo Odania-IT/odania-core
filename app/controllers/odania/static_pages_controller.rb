c0352209e40b1009620ba1acf3657463ec6ddcce
Mike Petersen <mike@odania-it.de>
2015-03-20 00:00:29 +0100

    Added before_action to static pages controller

--- app/controllers/odania/static_pages_controller.rb
+++ app/controllers/odania/static_pages_controller.rb
@@ -1,4 +1,6 @@
 class Odania::StaticPagesController < ApplicationController
+	before_action :valid_site!
+
 	def show
 		@static_page = current_site.static_pages.where(id: params[:id]).first
 		return render_not_found if @static_page.nil?
