3a4e9bd4ec4b035bd15baa5779ce203598955685
Mike Petersen <mike@odania-it.de>
2016-06-11 11:42:48 +0200

    Updated parsing elasticsearch config file

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -1,4 +1,4 @@
-config = YAML.load_file ERB.new(File.join(Rails.root, 'config', 'elasticsearch.yml')).result
+config = YAML.load(ERB.new(File.read(File.join(Rails.root, 'config', 'elasticsearch.yml'))).result)
 $elasticsearch = Elasticsearch::Client.new log: true, hosts: config[Rails.env]['hosts']
 
 $web_index = 'odania_web'
