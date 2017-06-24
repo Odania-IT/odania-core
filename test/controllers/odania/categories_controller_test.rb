94b7923755e6371a857d936615bad8d70a6b94c1
Mike Petersen <mike@odania-it.de>
2017-06-24 13:32:07 +0200

    Updated test helper

--- test/controllers/odania/categories_controller_test.rb
+++ test/controllers/odania/categories_controller_test.rb
@@ -4,13 +4,13 @@ module Odania
 	class CategoriesControllerTest < ActionDispatch::IntegrationTest
 		include Engine.routes.url_helpers
 
-		test "should get index" do
-			get categories_index_url
+		test 'should get index' do
+			get categories_url(locale: 'de')
 			assert_response :success
 		end
 
-		test "should get show" do
-			get categories_show_url
+		test 'should get show' do
+			get categories_url(locale: 'de', id: category.id)
 			assert_response :success
 		end
 
